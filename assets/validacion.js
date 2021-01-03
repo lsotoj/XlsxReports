968abfb027eba5bd3e3286709743448f51702f60
lsotoj <luissotoj@gmail.com>
2021-01-03 04:25:15 -0600

    Working with the NPM package csvtojson, the package was installed and I will work my csv file.

---
+++ assets/validacion.js
@@ -0,0 +1,6 @@
+function validando(objeto) {
+    console.log('si entre');
+    console.log(objeto);
+}
+
+module.exports = validando;
--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-const ExcelJS = require('exceljs');
+const csv = require('csvtojson');
+const fs = require('fs');
+const validacion = require('./assets/validacion');
+
+//DECLARTION OF THE STREAM
+// let readableStream = fs.createReadStream(__dirname + '/parque.csv');
+// readableStream.setEncoding('UTF8');
+// readableStream.pipe(csv());
+// console.log(readableStream);
+
+const csvFilePath = __dirname + '/parque.csv';
+csv()
+.fromFile(csvFilePath)
+.then((jsonObj) => {
+    jsonObj.forEach(elemento => {
+        console.log(elemento);
+    });
+    validacion(jsonObj[0]);
+    // fs.writeFile('File.txt', jsonObj, function (err) {
+    //     if (err) throw err;
+    //     console.log('file created');
+    // });
+});
+
